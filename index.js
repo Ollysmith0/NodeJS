e6128a5f1c415d07de91f3a02ed3f58b9c64a84d
Luke <ollysmith0@gmail.com>
2020-07-10 15:28:57 +0700

    express authentication add user

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+
+const userRoutes = require('./routes/user.route');
+
 const port = 4000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const app = express();
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'test', message: 'My name is Oil'
+    })
+})
+
+app.use('/users', userRoutes)
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
